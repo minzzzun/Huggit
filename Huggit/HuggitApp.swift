d483e7db4e71cf270a0b69227a3db0d0afeb99dc
minjun kim <minzzzun@gmail.com>
2025-02-19 15:14:26 +0900

    Initial commit

---
+++ Huggit/HuggitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuggitApp.swift
+//  Huggit
+//
+//  Created by 김민준 on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HuggitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HuggitTests/HuggitTests.swift
@@ -0,0 +1,17 @@
+//
+//  HuggitTests.swift
+//  HuggitTests
+//
+//  Created by 김민준 on 2/19/25.
+//
+
+import Testing
+@testable import Huggit
+
+struct HuggitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
