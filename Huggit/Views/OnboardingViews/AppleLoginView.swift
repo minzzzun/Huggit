39e72c2b89d85c8b69cc825f7289e03851734b3d
minjun kim <minzzzun@gmail.com>
2025-03-03 15:39:56 +0900

    [Feat] : AppleLoginView UI 구현완료

--- Huggit/Views/OnboardingViews/AppleLoginView.swift
+++ Huggit/Views/OnboardingViews/AppleLoginView.swift
@@ -5,49 +5,65 @@ struct AppleLoginView: View {
     @EnvironmentObject var router: NavigationRouter
     
     var body: some View {
-        VStack(spacing: 0) {
-            Spacer()
+        
+        
+        ZStack {
+            Rectangle()
+                .foregroundLinearGradient(
+                    colors: [
+                        Color(hex: "121317"),
+                        Color(hex: "3E4046")
+                    ],
+                    startPoint: .topTrailing,
+                    endPoint: .bottomLeading
+                )
+                .ignoresSafeArea()
             
-            // 텍스트 영역
-            VStack(alignment: .leading, spacing: 4) {
-                Text("개발자들의")
+            
+            VStack(spacing: 0) {
+                Spacer()
+                
+                // 텍스트 영역
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("개발자들의")
+                        .font(.system(size: 26, weight: .regular))
+                        .fontWeight(.semibold)
+                    HStack(spacing: 0) {
+                        Text("모든 성실")
+                            .foregroundColor(.blueButton)
+                        Text("을 담다,")
+                    }
                     .font(.system(size: 26, weight: .regular))
                     .fontWeight(.semibold)
-                HStack(spacing: 0) {
-                    Text("모든 성실")
-                        .foregroundColor(.blue)
-                    Text("을 담다,")
+                    
+                    Text("HUGGIT")
+                        .font(.system(size: 45, weight: .bold))
+                        .padding(.top, 4)
                 }
-                .font(.system(size: 26, weight: .regular))
-                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 20)
                 
-                Text("HUGGIT")
-                    .font(.system(size: 45, weight: .bold))
-                    .padding(.top, 4)
+                Spacer()
+                
+                // 이미지
+                Image("loginImage")
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width:254, height: 283)
+                
+                
+                Spacer()
+                
+                // 애플 로그인 버튼
+                AppleLoginButton()
+                    .padding(.bottom, 50)
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 20)
-            
-            Spacer()
-            
-            // 이미지
-            Image("loginImage")
-                .resizable()
-                .scaledToFill()
-                .clipped()
-                .frame(width:254, height: 283)
-
-            
-            Spacer()
-            
-            // 애플 로그인 버튼
-            AppleLoginButton()
-                .padding(.bottom, 50)
         }
+        .foregroundColor(Color.white)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.gray)
-        .foregroundColor(.white)
         .navigationBarHidden(true)
     }
+        
 }
 
