954bf5e89fd1faff095dbfdeca345fe6668ab831
K_Minhyeok <61306657+Kim-Min-Hyeok@users.noreply.github.com>
2025-05-11 16:32:35 +0900

    [Feat]: DesignSystems - Font

---
+++ Huggit/DesignSystems/Fonts/Font.swift
@@ -0,0 +1,44 @@
+//
+//  Font.swift
+//  Huggit
+//
+//  Created by Minhyeok Kim on 5/11/25.
+//
+
+import SwiftUI
+
+extension Font {
+    // MARK: - Heading
+    static var h126SB: Font { .custom("Pretendard-SemiBold", size: 26) }
+    static var h227SB: Font { .custom("Pretendard-SemiBold", size: 27) }
+    static var h323SB: Font { .custom("Pretendard-SemiBold", size: 23) }
+    static var h421M:  Font { .custom("Pretendard-Medium", size: 21) }
+    static var h518SB: Font { .custom("Pretendard-SemiBold", size: 18) }
+    static var h615M:  Font { .custom("Pretendard-Medium", size: 15) }
+
+    // MARK: - Subtitle
+    static var s114M: Font { .custom("Pretendard-Medium", size: 14) }
+    static var s213M: Font { .custom("Pretendard-Medium", size: 13) }
+
+    // MARK: - Display
+    static var d115M: Font { .custom("Pretendard-Medium", size: 15) }
+    static var d222L: Font { .custom("Pretendard-Light", size: 22) }
+    static var d39M:  Font { .custom("Pretendard-Medium", size: 9)  }
+    static var d415R: Font { .custom("Pretendard-Regular", size: 15) }
+    static var d515B: Font { .custom("Pretendard-Bold", size: 15)   }
+    static var d611M: Font { .custom("Pretendard-Medium", size: 11) }
+    static var d714L: Font { .custom("Pretendard-Light", size: 14)  }
+
+    // MARK: - Button
+    static var b117SB: Font { .custom("Pretendard-SemiBold", size: 17) }
+    static var b213R: Font { .custom("Pretendard-Regular", size: 13)   }
+
+    // MARK: - Calendar
+    static var c114R: Font { .custom("Pretendard-Regular", size: 14) }
+    static var c29R:  Font { .custom("Pretendard-Regular", size: 9)  }
+    static var c312R: Font { .custom("Pretendard-Regular", size: 12) }
+    static var c411R: Font { .custom("Pretendard-Regular", size: 11) }
+    static var c514SB: Font { .custom("Pretendard-SemiBold", size: 14) }
+    static var c67M:  Font { .custom("Paperlogy-Medium", size: 7)    }
+    static var c712SB:Font { .custom("Pretendard-SemiBold", size: 12) }
+}
