8543c76cf57c7b2091c19587fcdb7891ff3dc9e0
K_Minhyeok <61306657+Kim-Min-Hyeok@users.noreply.github.com>
2025-05-20 16:48:24 +0900

    [Feat]: 로그아웃 모달, [Fix]: Github 로그인 메인 스레드 에러 수정

--- Huggit/Views/MyPageViews/MyPageView.swift
+++ Huggit/Views/MyPageViews/MyPageView.swift
@@ -10,41 +10,60 @@ import SwiftUI
 struct MyPageView: View {
     @StateObject var myPageViewModel = MyPageViewModel()
     @EnvironmentObject var router: NavigationRouter
-
+    
+    @State private var showLogoutModal = false
+    
     var body: some View {
-        ScrollView(.vertical) {
-            VStack {
-                // 앱바
-                AppBarView(
-                    isHomeView: false,
-                    title: "마이페이지",
-                    buttonImage: "house",
-                    action: {
-                        router.offAll("/")
-                    }
-                )
-                
-                // 마이페이지_유저 이름 & 커밋 수
-                UserInfoView()
-                    .padding(.top, 15)
-                
-                // 피드백 설문 폼
-                FeedbackView()
-                    .padding(.top, 20)
-                
-                // 계정 정보
-                AccountInfoView()
-                    .padding(.top, 40)
-                
-                // 이용 안내
-                UseInfoView()
-                    .padding(.top, 40)
-                
+        ZStack {
+            ScrollView(.vertical) {
+                VStack {
+                    // 앱바
+                    AppBarView(
+                        isHomeView: false,
+                        title: "마이페이지",
+                        buttonImage: "house",
+                        action: {
+                            router.offAll("/")
+                        }
+                    )
+                    
+                    // 마이페이지_유저 이름 & 커밋 수
+                    UserInfoView()
+                        .padding(.top, 15)
+                    
+                    // 피드백 설문 폼
+                    FeedbackView()
+                        .padding(.top, 20)
+                    
+                    // 계정 정보
+                    AccountInfoView()
+                        .padding(.top, 40)
+                    
+                    // 이용 안내
+                    UseInfoView(showLogoutModal: $showLogoutModal)
+                                            .padding(.top, 40)
+                    
+                }
+                .padding(.horizontal, 20)
             }
-            .padding(.horizontal, 20)
+            .background(Color.primaryDarkBlue)
+            .environmentObject(myPageViewModel)
+            .navigationBarHidden(true)
+            
+            // 로그아웃 모달
+            if showLogoutModal {
+                Color.black.opacity(0.8)
+                    .edgesIgnoringSafeArea(.all)
+                    .onTapGesture {
+                        showLogoutModal = false
+                    }
+                LogoutModalView(isPresented: $showLogoutModal) {
+                    myPageViewModel.logout {
+                        router.offAll("/appleLogin")
+                    }
+                }
+                .padding(.horizontal, 20)
+            }
         }
-        .background(Color.primaryDarkBlue)
-        .environmentObject(myPageViewModel)
-        .navigationBarHidden(true)
     }
 }
